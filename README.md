f30d4bb4bc433d1059c93e47da38a6563c901480
Nguyen Cong Minh <minhnc@deha-soft.com>
2024-04-04 23:22:17 +0700

    refactor folder

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ## Project Mini Of NCM
+
+#### Config firebase
+
+```
+firebase login
+
+firebase init hosting
+
+firebase deploy --only hosting
+```
---
+++ src/heartbeat/libraryHeartbeat/js/script.js
@@ -0,0 +1,130 @@
+/*
+ * Settings
+ */
+var settings = {
+  particles: {
+    length: 2000, // maximum amount of particles
+    duration: 2, // particle duration in sec
+    velocity: 100, // particle velocity in pixels/sec
+    effect: -1.3, // play with this for a nice effect
+    size: 13, // particle size in pixels
+  },
+};
+/*
+ * RequestAnimationFrame polyfill by Erik Möller
+ */
+/*
+ * Point class
+ */
+var Point = (function () {
+  function Point(x, y) {
+    this.x = typeof x !== "undefined" ? x : 0;
+    this.y = typeof y !== "undefined" ? y : 0;
+  }
+  Point.prototype.clone = function () {
+    return new Point(this.x, this.y);
+  };
+  Point.prototype.length = function (length) {
+    if (typeof length == "undefined") return Math.sqrt(this.x * this.x + this.y * this.y);
+    this.normalize();
+    this.x *= length;
+    this.y *= length;
+    return this;
+  };
+  Point.prototype.normalize = function () {
+    var length = this.length();
+    this.x /= length;
+    this.y /= length;
+    return this;
+  };
+  return Point;
+})();
+/*
+ * Particle class
+ */
+var Particle = (function () {
+  function Particle() {
+    this.position = new Point();
+    this.velocity = new Point();
+    this.acceleration = new Point();
+    this.age = 0;
+  }
+  Particle.prototype.initialize = function (x, y, dx, dy) {
+    this.position.x = x;
+    this.position.y = y;
+    this.velocity.x = dx;
+    this.velocity.y = dy;
+    this.acceleration.x = dx * settings.particles.effect;
+    this.acceleration.y = dy * settings.particles.effect;
+    this.age = 0;
+  };
+  Particle.prototype.update = function (deltaTime) {
+    this.position.x += this.velocity.x * deltaTime;
+    this.position.y += this.velocity.y * deltaTime;
+    this.velocity.x += this.acceleration.x * deltaTime;
+    this.velocity.y += this.acceleration.y * deltaTime;
+    this.age += deltaTime;
+  };
+  Particle.prototype.draw = function (context, image) {
+    function ease(t) {
+      return --t * t * t + 1;
+    }
+    var size = image.width * ease(this.age / settings.particles.duration);
+    context.globalAlpha = 1 - this.age / settings.particles.duration;
+    context.drawImage(image, this.position.x - size / 2, this.position.y - size / 2, size, size);
+  };
+  return Particle;
+})();
+/*
+ * ParticlePool class
+ */
+var ParticlePool = (function () {
+  var particles,
+    firstActive = 0,
+    firstFree = 0,
+    duration = settings.particles.duration;
+
+  function ParticlePool(length) {
+    // create and populate particle pool
+    particles = new Array(length);
+    for (var i = 0; i < particles.length; i++) particles[i] = new Particle();
+  }
+  ParticlePool.prototype.add = function (x, y, dx, dy) {
+    particles[firstFree].initialize(x, y, dx, dy);
+
+    // handle circular queue
+    firstFree++;
+    if (firstFree == particles.length) firstFree = 0;
+    if (firstActive == firstFree) firstActive++;
+    if (firstActive == particles.length) firstActive = 0;
+  };
+  ParticlePool.prototype.update = function (deltaTime) {
+    var i;
+
+    // update active particles
+    if (firstActive < firstFree) {
+      for (i = firstActive; i < firstFree; i++) particles[i].update(deltaTime);
+    }
+    if (firstFree < firstActive) {
+      for (i = firstActive; i < particles.length; i++) particles[i].update(deltaTime);
+      for (i = 0; i < firstFree; i++) particles[i].update(deltaTime);
+    }
+
+    // remove inactive particles
+    while (particles[firstActive].age >= duration && firstActive != firstFree) {
+      firstActive++;
+      if (firstActive == particles.length) firstActive = 0;
+    }
+  };
+  ParticlePool.prototype.draw = function (context, image) {
+    // draw active particles
+    if (firstActive < firstFree) {
+      for (let i = firstActive; i < firstFree; i++) particles[i].draw(context, image);
+    }
+    if (firstFree < firstActive) {
+      for (let i = firstActive; i < particles.length; i++) particles[i].draw(context, image);
+      for (let i = 0; i < firstFree; i++) particles[i].draw(context, image);
+    }
+  };
+  return ParticlePool;
+})();
