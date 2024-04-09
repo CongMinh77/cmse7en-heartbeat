e9936ea80d1dfb29791ddeb1954574fe76c2c508
Nguyen Cong Minh <minhnc@deha-soft.com>
2024-04-09 22:18:18 +0700

    change design

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,32 @@
 $(document).ready(function () {
   const element = {
     title: $(".title"),
+    progress: $("#progress"),
+    wrapMessage: $("#wrapMessage"),
+    wrapProgressBar: $("#wrapProgressBar"),
+    cancelButton: $("#cancelButton"),
+    main: $("#main"),
   };
 
-  element.title.on("click", function () {
-    console.debug("helllo");
+  const timeProgress = 5000;
+
+  element.progress.animate({ width: "100%" }, timeProgress);
+
+  setInterval(function () {
+    element.wrapProgressBar.css("display", "none");
+    element.wrapMessage.css("display", "inline-block");
+  }, timeProgress);
+
+  element.cancelButton.on("click", function () {
+    axios({
+      method: "get",
+      url: "http://127.0.0.1:5500/src/HeartbeatWebs/LibraryHeartbeat/heartType6.html",
+      responseType: "stream",
+    }).then(function (response) {
+      const a = response.data;
+      element.main.html(a);
+
+      console.debug("a", a);
+    });
   });
 });
